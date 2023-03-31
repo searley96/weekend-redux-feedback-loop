2991b45be8d14f198037463b8281dfecff164bc0
Siv Earley <searley96@gmail.com>
2023-03-31 15:34:39 -0500

    added routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
 
@@ -10,6 +11,27 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route path="/">
+          <Feelings />
+        </Route>
+        <Route path="/understanding">
+         <Understanding />
+        </Route>
+        <Route path="/supported">
+          <Supported />
+        </Route>
+        <Route path="/comments">
+          <Comments />
+        </Route>
+        <Route path="/review">
+          <Review />
+        </Route>
+        <Route path="/feedback">
+          <Feedback />
+        </Route>
+      </Router>
+
     </div>
   );
 }
